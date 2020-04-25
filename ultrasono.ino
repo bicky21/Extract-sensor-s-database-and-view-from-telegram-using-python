154b6d0997a9a9d495bc0abd9e3f99cade334b74
bicky21 <33664249+bicky21@users.noreply.github.com>
2020-04-25 14:21:53 +0530

    Update ultrasono.ino

--- ultrasono.ino
+++ ultrasono.ino
@@ -11,10 +11,8 @@ void setup() {
     pinMode(echoPin,INPUT);
     Serial.begin(9600);
     Serial.setTimeout(50);
-    boltiot.begin(Serial); //Initialize the Bolt interface over serial uart. Serial could be replaced with Serial0 or Serial1 on Arduino mega boards.
-         //In this example Tx pin of Bolt is connected to Rx pin of Arduino Serial Port
-         //and Rx pin of Bolt is connected to tx pin of arduino Serial Port
-    pinMode(2,INPUT); //Set pin 2 as input. We will send this pin's state as the data to the Bolt Cloud
+    boltiot.begin(Serial); //Communication
+    pinMode(2,INPUT);
   }
 
   void loop() {
@@ -25,7 +23,7 @@ void setup() {
      digitalWrite(trigPin, LOW);
      duration = pulseIn(echoPin, HIGH);
      distanceCm = duration * 0.0340 / 2;
-     Serial.print(distanceCm,DEC);
+     Serial.print(distanceCm);
      Serial.print("\n");
      delay(5000);
   }
