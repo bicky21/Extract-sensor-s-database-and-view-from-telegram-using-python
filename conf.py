81f085497b48af46e686f2d677633f380dadcb0d
bicky21 <33664249+bicky21@users.noreply.github.com>
2020-04-25 23:29:50 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,3 +1,2 @@
-bolt_api_key = "YOUR BOLT API"               # This is your Bolt Cloud API Key
-device_id = "DEVICE ID"                  # This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
-telegram_chat_id = "TELEGRAM CHAT ID"          # This is the channel ID of the created Telegram channel. Paste after @ 
+bolt_api_key = "YOUR BOLT API" #Your bolt API             
+device_id = "DEVICE ID"   #Your device ID                        
